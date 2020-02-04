2cf3f1d3937b40d8c6f90aa972413e02d1b0bf40
ihor melnyk <ih.melnyk@gmail.com>
2020-02-04 17:14:30 +0200

    Changing from Python2 to Python3

--- models/facemodel/facemodel.py
+++ models/facemodel/facemodel.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-
 import numpy
 import math
 import lutorpy
@@ -9,51 +7,58 @@ import os.path
 import get_attrs
 import cPickle as pickle
 
-def center_crop(X,crop):
-  '''X is N x H x W x 3'''
-  h1=int(math.ceil((X.shape[1]-crop)/2))
-  w1=int(math.ceil((X.shape[2]-crop)/2))
-  return X[:,h1:h1+crop,w1:w1+crop,:]
+
+def center_crop(X, crop):
+    '''X is N x H x W x 3'''
+    h1 = int(math.ceil((X.shape[1] - crop) / 2))
+    w1 = int(math.ceil((X.shape[2] - crop) / 2))
+    return X[:, h1:h1 + crop, w1:w1 + crop, :]
+
 
 require('cutorch')
 require('cunn')
 require('cudnn')
 
+
 class FaceModel:
-  def __init__(self,modelfile='model.t7'):
-    '''
+    def __init__(self, modelfile='model.t7'):
+        '''
     This model takes a 160x160 input which is a single face aligned to
     self.celeba_template (which is a set of 68 dlib landmarks). Call
     preprocess() then forward() to get a 1024-dim feature vector. Then
     call predict_attributes() on the features to get 40 attribute
     decision values.
     '''
-    self.model=torch.load('model.t7')
-    self.model._evaluate() # set model to test mode
-    self.meanstd={'mean':numpy.array([0.485, 0.456, 0.406]),'std':numpy.array([0.229, 0.224, 0.225])}
-    self.celeba_template=numpy.load('celeba_dlib_template.npz')['template']
-    self.clf={k:pickle.load(open('classifiers/{}.pkl'.format(k), 'rb')) for k in get_attrs.fields}
-    self.fields=tuple(get_attrs.fields)
-  def preprocess(self,X):
-    '''X is an N x H x W x 3 set of images in the range [0, 1].'''
-    X=X-self.meanstd['mean']
-    X=X/self.meanstd['std']
-    X=center_crop(X,160)
-    return X
-  def forward(self,X):
-    '''X is an N x H x W x 3 set of images'''
-    thX=torch.fromNumpyArray(X.transpose(0,3,1,2))
-    cuthX=thX._cuda()
-    assert torch.typename(cuthX) in cudnn.typemap.keys()
-    thY=self.model._forward(cuthX)
-    Y=thY.asNumpyArray()
-    return Y
-  def predict_attributes(self,Y):
-    '''Returns N x K decision values. K is |self.fields|.'''
-    scores=numpy.array([self.clf[k].decision_function(Y) for k in self.fields]).T
-    return scores
+        self.model = torch.load('model.t7')
+        self.model._evaluate()  # set model to test mode
+        self.meanstd = {'mean': numpy.array([0.485, 0.456, 0.406]), 'std': numpy.array([0.229, 0.224, 0.225])}
+        self.celeba_template = numpy.load('celeba_dlib_template.npz')['template']
+        self.clf = {k: pickle.load(open('classifiers/{}.pkl'.format(k), 'rb')) for k in get_attrs.fields}
+        self.fields = tuple(get_attrs.fields)
 
-__doc__='''
+    def preprocess(self, X):
+        '''X is an N x H x W x 3 set of images in the range [0, 1].'''
+        X = X - self.meanstd['mean']
+        X = X / self.meanstd['std']
+        X = center_crop(X, 160)
+        return X
+
+    def forward(self, X):
+        '''X is an N x H x W x 3 set of images'''
+        thX = torch.fromNumpyArray(X.transpose(0, 3, 1, 2))
+        cuthX = thX._cuda()
+        assert torch.typename(cuthX) in cudnn.typemap.keys()
+        thY = self.model._forward(cuthX)
+        Y = thY.asNumpyArray()
+        return Y
+
+    def predict_attributes(self, Y):
+        '''Returns N x K decision values. K is |self.fields|.'''
+        scores = numpy.array([self.clf[k].decision_function(Y) for k in self.fields]).T
+        return scores
+
+
+__doc__ = '''
 Command-line tools:
 
 th extract_features.lua -data IMAGEDIR -save OUTPUTDIR
@@ -94,4 +99,3 @@ for x in scores:
 for x in scores:
   print ', '.join(['{} ({})'.format(k,x[i]) for i,k in enumerate(fm.fields) if x[i]<=0])
 '''
-
